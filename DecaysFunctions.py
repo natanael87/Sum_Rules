38e260e6a5e757a4caac6095a969fd1162e7de25
georgefcoral <georgefcoral@gmail.com>
2021-06-28 18:35:00 -0500

    Se añadieron versiones implementadas en Spyder.
    Requerimientos: 8x8comp2.txt, DecaysFunctions.py.
    Correr main.py

---
+++ DecaysFunctions.py
@@ -0,0 +1,275 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 28 12:14:21 2021
+
+@author: George Coral
+Funciones para Case 8x8
+"""
+import pandas as pd
+import numpy as np
+import math
+
+from sympy import *
+from sympy.parsing.sympy_parser import parse_expr
+
+# Calcula el cgc para un caso específico dentro de un df previamente filtrado
+def cgc_md(initial1, initial2, final, ddf, df):
+    dff = ddf.loc[(df['k1']==initial1[0])&(df['l1']==initial1[1])&(df['m1']==initial1[2])]
+    dff = dff.loc[(df['k2']==initial2[0])&(df['l2']==initial2[1])&(df['m2']==initial2[2])]
+    dff = dff.loc[(df['k']==final[0])&(df['l']==final[1])&(df['m']==final[2])]
+
+    cgc_str=dff.loc[:,'cg_coef'].values[0]
+    cgc = parse_expr(cgc_str, evaluate=0)
+    return cgc
+
+# Verifica si el df filtrado es simétrico o antisimétrico
+def is_symmetric(sdf, df):
+    p1 = sdf[['k1','l1','m1']].head(1).values[0]
+    p2 = sdf[['k2','l2', 'm2']].head(1).values[0]
+    pf = sdf[['k', 'l', 'm']].head(1).values[0]
+    val1 = cgc_md(p1, p2, pf, sdf, df)
+    val2 = cgc_md(p2, p1, pf, sdf, df)
+    if val1 == val2:
+        return 'S'
+    else:
+        return 'A'
+    
+    
+# Retorna las degeneraciones de SU(2) dado un valor de I
+def dec_su2(nmax):
+    row = []
+    
+    rowImax = nmax
+    size_m = 2*nmax + 1
+    for i in range(size_m):
+        row.insert(0,nmax)
+        nmax = nmax -1
+    return row
+
+# Retorna el grupo de partículas pertenecientes al nivel solicitado
+def level_particles(level,Imax,Ymax,e):
+    # Retorna la lista de la linea más larga en todo el multiplete
+    I3max_line = dec_su2(Imax) # es una lista
+
+    subIsospin3 = []
+    subIsospin = []
+    subHipercharge = []
+    
+    # Delimita los valores máximos y mínimos por nivel horizontal
+    a = level-1
+    b = len(I3max_line)-level + 1
+    subIsospin3.append(I3max_line[a:b])
+    Imax_line = I3max_line[b-1] # es un número
+    list_subI = []
+    list_subI.append(Imax_line)
+    list_subI = list_subI*(b-a)
+    subIsospin.append(list_subI)
+    subImax = I3max_line[-level]
+    list_subY=[]
+    list_subY.append(Ymax)
+    list_subY = list_subY*(b-a)
+    subHipercharge.append(list_subY)
+    
+    # Recorre sobre la línea vertical hacia abajo formando grupos de partículas
+    prow = []
+    Inew = subImax
+    Yrow1 = []
+    Ynew1 = Ymax
+    
+    for i in range(e[0]-level+1):
+        Inew = nsimplify(Inew -1/2)
+        prow.append(Inew)
+        Ynew1 = nsimplify(Ynew1 - 1)
+        Yrow1.append(Ynew1)
+    
+    for i in range(len(prow)):
+        subIsospin3.append(dec_su2(prow[i]))
+        
+        len_p = len(I3max_line)-i-1
+        list_subI = []
+        list_subI.append(prow[i])
+        list_subI = list_subI*len_p
+        subIsospin.append(list_subI)
+        
+        list_subY=[]
+        list_subY.append(Yrow1[i])
+        list_subY = list_subY*len_p
+        subHipercharge.append(list_subY)
+    
+    # Recorre sobre la línea vertical hacia arriba formando grupos de partículas
+    qrow = []
+    Inew2 = subImax
+    Yrow2 = []
+    Ynew2 = Ymax
+    for i in range(e[1]-level+1):
+        Inew2 = nsimplify(Inew2 -1/2)
+        qrow.insert(i,Inew2)
+        Ynew2 = nsimplify(Ynew2 + 1)
+        Yrow2.append(Ynew2)
+        
+    for i in range(len(qrow)):
+        
+        subIsospin3.insert(0,dec_su2(qrow[i]))
+        list_subI = []
+        list_subI.append(qrow[i])
+        len_q = len(I3max_line)-i-1
+        list_subI = list_subI*len_q
+        subIsospin.insert(0,list_subI)
+        
+        list_subY=[]
+        list_subY.append(Yrow2[i])
+        list_subY = list_subY*len_q
+        subHipercharge.insert(0, list_subY)
+        
+    return subHipercharge, subIsospin, subIsospin3
+
+# Retorna el número de partículas en el grupo del nivel solicitado
+def count_particles(levelp):
+    tot_particles = 0
+    for i in range(len(levelp)):
+        tot_particles = tot_particles + len(levelp[i])
+    return tot_particles
+
+
+# Transforma los números cuánticos de una particula de y, i, iz al tipo k, l, m
+def transform(particle,e):
+    # Se escoge el multiplete con que se va a trabajar
+    p = e[0]
+    q = e[1]
+    
+    # Convierte enteros k, l, m
+    k = int((p+2*q)/3 + particle[0]/2 + particle[1])
+    l = int((p+2*q)/3 + particle[0]/2 - particle[1])
+    m = int((p+2*q)/3 + particle[0]/2 + particle[2])
+    return k,l,m
+
+# Recibe una lista de lista de numeros y, i, iz la cambia a lista de enteros k,l,m
+def transform_m(listm,e):
+    list_klm = []
+    for i in range(len(listm)):
+        list_klm.append(transform(listm[i],e))
+    array_klm = np.array(list_klm)
+    return array_klm
+
+
+
+def transformpq(particle, epq):
+    # Se escoge el multiplete con que se va a trabajar
+    p = epq[0]
+    q = epq[1]
+    
+    # Convierte enteros k, l, m
+    k = int((p+2*q)/3 + particle[0]/2 + particle[1])
+    l = int((p+2*q)/3 + particle[0]/2 - particle[1])
+    m = int((p+2*q)/3 + particle[0]/2 + particle[2])
+    return k,l,m
+
+def get_filters(df_describe):
+    dictini = df_describe.to_dict('list')
+    dictfinal = df_describe.to_dict('list')
+    del dictini['dtot']
+    del dictfinal['dtot']
+
+    # Forma el df Inicial
+    for i in range(df_describe.shape[0]):
+        if df_describe.iloc[i,3]>1:
+            for j in range(df_describe.iloc[i,3]-1):
+                dictini['p'].insert(i+(df_describe.iloc[i,3]-1)*(j+1), df_describe.iloc[i,0])
+                dictini['q'].insert(i+(df_describe.iloc[i,3]-1)*(j+1), df_describe.iloc[i,1])
+                dictini['degeneracy'].insert(i, df_describe.iloc[i,2])
+                dictini['is_symm'].insert(i+(df_describe.iloc[i,3]-1)*(j+1), df_describe.iloc[i,4])
+
+    dfIni = pd.DataFrame(dictini)
+
+    # Forma el df Final
+    for i in range(df_describe.shape[0]):
+        if df_describe.iloc[i,3]>1:
+            for j in range(df_describe.iloc[i,3]-1):
+                dictfinal['p'].insert(i, df_describe.iloc[i,0])
+                dictfinal['q'].insert(i, df_describe.iloc[i,1])
+                dictfinal['degeneracy'].insert(i+(df_describe.iloc[i,3]-1)*(j+1), df_describe.iloc[i,2])
+                dictfinal['is_symm'].insert(i+(df_describe.iloc[i,3]-1)*(j+1), df_describe.iloc[i,4])
+    return pd.DataFrame(dictini),pd.DataFrame(dictfinal)
+
+# Considerando el valor de multiplets se calculan k, l, m dependiendo de p y q
+def Di_pq(Dklm,multiplets):
+    Dpq_l=[]
+    for j in range(len(multiplets)):
+        Dpq_l.append(transformpq(Dklm, multiplets[j]))
+    Dpq = np.array(Dpq_l)
+    return Dpq
+
+#Crea un diccionario donde contiene un conjunto general de llaves que incluyen 
+def getDictConversion(mB,multiplets,e0klm):
+    multiplets_dict = {}
+    dictConversion = {}
+    for m in range(len(mB)):
+        DipqArray = Di_pq(mB[m],multiplets)
+        for i in range(len(multiplets)):
+            multiplets_dict.update({tuple(multiplets[i]):tuple(DipqArray[i])})
+        dictConversion .update({tuple(e0klm[m]): multiplets_dict})
+        multiplets_dict = {}
+    return dictConversion
+
+
+
+def get_cgDecays(mB,multiplets,e0klm,df,dfI_info,dfF_info):
+    cgIni = 0
+    sizeI = len(dfI_info)
+    sizeF = len(dfF_info)
+    pI = list(dfI_info.iloc[range(0,sizeI),0])
+    qI = list(dfI_info.iloc[range(0,sizeI),1])
+    pF = list (dfF_info.iloc[range(0,sizeF),0])
+    qF = list (dfF_info.iloc[range(0,sizeF),1])
+    #diccionario de klm dado un mB.
+    count = 0
+    matrizFinal = Matrix([])
+    dictConversion = getDictConversion(mB,multiplets,e0klm)
+    for i in range(0,len(e0klm)):
+        print("count",count)
+        colum = []
+        for j in range(sizeI):
+            #print("pI[j]: ",pI[j],"qI[j]: ",qI[j])
+            klm = list(dictConversion[tuple(e0klm[i])][tuple([pI[j],qI[j]])])
+            #print("i = ",i," j = ", j)
+            #print([e0klm[i],klm,len(e0klm)])
+            #print("p: ",pI[j],"------"," q: ",qI[j])
+            df11=df.loc[(df['k']==klm[0])&(df['l']==klm[1])&(df['m']==klm[2])]
+            df11=df11.loc[(df['p']==pI[j]) & (df['q']==qI[j])]
+            df11=df11.loc[df['degeneracy']==dfI_info.iloc[j,2]]
+            df11=df11.loc[(df['k1']==1)&(df['l1']==1)&(df['m1']==1)]
+            
+            #print(df11)
+            #Salvamos valor encontrado:
+            if(df11.empty):
+                cgIni = 0
+            else:
+                cgIni = parse_expr(df11['cg_coef'].values[0],evaluate=0) 
+            
+            print("cgIni = ",cgIni)
+                
+            #Inicia ciclo para decaimiento final:
+            klm2 = list(dictConversion[tuple(e0klm[i])][tuple([pF[j],qF[j]])])
+            #print(klm2)
+            print("p: ",pF[j],"------"," q: ",qF[j])
+            df22=df.loc[(df['k']==klm2[0])&(df['l']==klm2[1])&(df['m']==klm2[2])]
+            df22=df22.loc[(df['p']==pF[j])&(df['q']==qF[j])]
+            df22=df22.loc[df['degeneracy']==dfF_info.iloc[j,2]]
+            #print("i = ",i," j = ", j, " sizeI ", sizeI)
+            #print(df22)
+            if(df22.empty or df11.empty ):
+                colum.append(-1)
+                print("Vacio!")
+            else:
+                for k in range(df22['cg_coef'].shape[0]):
+                    colum.append(Mul(parse_expr(df22.cg_coef.iloc[k]),cgIni))
+                    print("df22.cg_coef.iloc[",k,"]- = ",Mul(parse_expr(df22.cg_coef.iloc[k]),cgIni))
+            print("-------------------------------------------------------")
+                    
+        #print(Matrix(colum))
+        matrizFinal= matrizFinal.row_insert(count,Matrix(colum))
+        count +=1
+    return matrizFinal
+
+
+
---
+++ main.py
@@ -0,0 +1,175 @@
+import pandas as pd
+import numpy as np
+import math
+import DecaysFunctions as dcf
+
+from sympy import *
+from sympy.parsing.sympy_parser import parse_expr
+
+
+#__________IMPORTACION DE LOS DATOS___________
+# Se importa el archivo
+df = pd.read_csv('8x8comp2.txt', skiprows=4, header=None)
+
+# Se agregan las cabeceras de las columnas
+df.columns = ['k', 'l', 'm', 'k1', 'l1', 'm1', 'k2', 'l2', 'm2', 'cg_coef', 'p', 'q', 'degeneracy', 'dtot']
+
+# Se eliminan las filas con valores NaN
+df = df.dropna()
+
+# Convierte todas las columnas a tipo int con excepción de "cg_doef"
+df = df.astype({"degeneracy":int, "dtot":int})
+df.iloc[:,list(range(0,9))]=df.iloc[:,list(range(0,9))].astype(int)
+df[['p', 'q']] = df[['p', 'q']].astype(int)
+
+# Retorna un arreglo de arreglos de números p,q representando los multipletes existentes
+multiplets = df[['p','q']].drop_duplicates().values
+
+
+
+
+Symmetry = []
+Multiplets = [] 
+Degeneracy = []
+Dtot = []
+Index = []
+for i in range(len(multiplets)):
+    # Retorna un df del multiplete indicado en la función
+    m = df.loc[(df['p']==multiplets[i,0])&(df['q']==multiplets[i,1])]
+    
+    # Retorna una lista con las degeneraciones del multiplete m
+    degeneracy = m['degeneracy'].value_counts().index.tolist()
+    
+    # Recolecta los valores de dtot en cada multiplete
+    dtot = m['dtot'].value_counts().index.tolist()
+    
+    if len(degeneracy)>1:
+        for j in range(len(degeneracy)):
+            Multiplets.append([multiplets[i,0], multiplets[i,1]])
+            
+            # Divide el multiplete en df por degeneraciones
+            d = m.loc[df['degeneracy']==degeneracy[j]]
+            
+            Index.append(d.index)
+            Symmetry.append(dcf.is_symmetric(d,df))
+            Degeneracy.append(degeneracy[j])
+            # Repite los valores de dtot por si hay degeneración
+            Dtot.append(dtot[0])
+
+    else:
+        Index.append(m.index)
+        Multiplets.append([multiplets[i,0], multiplets[i,1]])
+        Symmetry.append(dcf.is_symmetric(m,df))
+        Degeneracy.append(degeneracy[0])
+        Dtot.append(dtot[0])
+
+# Elabora un df para describir cada multiplete
+df_describe = pd.DataFrame()
+MultipletsA = np.array(Multiplets)
+df_describe['p'] = MultipletsA[:,0]
+df_describe['q'] = MultipletsA[:,1]
+df_describe['degeneracy'] = Degeneracy
+df_describe['dtot'] = Dtot
+df_describe['is_symm'] = Symmetry
+print(df_describe)
+
+
+
+
+# Agrega una columna extra al df original
+extra =[]
+for i in range(df.shape[0]):
+    extra.append('x')
+df.loc[:,'is_symm']=extra
+
+# Asigna correctamente la característica si es simétrico o antisimétrico
+for i in range(len(Index)):
+    for j in Index[i]:
+        df.at[Index[i],'is_symm']=df_describe.loc[i, 'is_symm']
+        
+e = Matrix([1,1]) # Dibarión inicial
+e1 = Matrix([1,1]) # Barión final 1
+e2 = Matrix([1,1]) # Barión final 2  
+
+dimension = (1+e[0])*(1+e[1])*(2+sum(e))/2
+if e[0]<e[1]:
+    print('dimension ', dimension, '*')
+else:
+    print('dimension ', dimension)
+Imax = (e[0]+e[1])/2
+print('Imax ', Imax)
+Ymax = (e[0]-e[1])/3
+print('Ymax ', Ymax)
+Range = 2*Imax +1
+print('Range ', Range)
+if e1==e2:
+    print('S and A')
+    
+I3 = []
+I = []
+Y = []
+npart = 0
+for i in range(dimension):
+    level = i+1
+    subY, subI, subI3 = dcf.level_particles(level,Imax,Ymax,e)
+    npart = npart + dcf.count_particles(subI3)
+    I3.append(subI3)
+    I.append(subI)
+    Y.append(subY)
+    
+    if npart == dimension:
+        break
+    
+mB = []
+for i in range(len(Y)):
+    for j in range(len(Y[i])):
+        for k in range(len(Y[i][j])):
+            mB.append([Y[i][j][k], I[i][j][k], I3[i][j][k]])
+#print("mB:",mB)
+        
+# Se transforman los números Y, I, I3 a k, l, m de todas las partículas de los multipletes
+e0klm = dcf.transform_m(mB,e)
+e1klm = dcf.transform_m(mB,e)
+e2klm = dcf.transform_m(mB,e)
+
+#print("e0klm:\n", e0klm, "  \ne1klm:\n",e1klm, " \ne2klm:\n",e2klm)
+
+
+Gm = Matrix([])
+count = 0
+for i in range(len(e0klm)):
+    dfLS=df.loc[(df['is_symm']=='S')]
+    dfLS=dfLS.loc[(df['p']==e[0])&(df['q']==e[1])]
+    dfLS=dfLS.loc[(df['k']==e0klm[i,0])&(df['l']==e0klm[i,1])&(df['m']==e0klm[i,2])]
+    #print(e[0],"-----",e[1])
+   
+    #print(dfLS.shape[0])
+    for j in range(dfLS.shape[0]):
+        D=list(dfLS.iloc[j,0:3].values)
+        B1=list(dfLS.iloc[j,3:6].values)
+        B2=list(dfLS.iloc[j,6:9].values)
+        cgci = dcf.cgc_md(B1,B2,D,dfLS,df)
+        tag = [D, B1, B2]
+        Gi = 'G'+str(count)
+        count = count + 1
+        Gm = Gm.row_insert(count, Matrix([[cgci,tag, Gi]]))
+        
+        
+        
+        
+dfIni,dfFinal = dcf.get_filters(df_describe)
+
+# Separa cada df en sus partes simétricas y antisimétricas
+dfIS=dfIni.loc[(dfIni['is_symm']=='S')]
+dfIA=dfIni.loc[(dfIni['is_symm']=='A')]
+dfFS=dfFinal.loc[(dfFinal['is_symm']=='S')]
+dfFA=dfFinal.loc[(dfFinal['is_symm']=='A')]
+
+dictConversion = dcf.getDictConversion(mB,multiplets,e0klm)
+
+
+matrizFinal = dcf.get_cgDecays(mB,multiplets,e0klm,df,dfIS,dfFS)
+
+
+    
+
